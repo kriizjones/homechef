deeb238c79e83108667177aa8ca926da50e9a6d1
rmzse <rodrigo@therawdelicious.com>
2017-01-10 23:49:50 +0100

    add test to find the logotype placeholder, test failing

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -10,8 +10,13 @@ Then(/^I should see a placeholder for dishes in general with an id of "([^"]*)"$
   expect(page).to have_selector(:css, "div##{id}")
 end
 
+Then(/^I should see a "([^"]*)" placeholder$/) do |id|
+  expect(page).to have_selector(:css, "div##{id}")
+end
+
 Then(/^I should see a link called "([^"]*)" that links to the main landing page$/) do |link|
   expect(page).to have_link link
   click_link link
 	%q{ When I visit the main landing page }
 end
+
