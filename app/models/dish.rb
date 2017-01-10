c01f6e6105011b5f709177e18cc143842472cf91
aniansson <ebba@aniansson.se>
2017-01-10 17:11:48 +0100

    Create Rspec tests for dish and make them pass

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,2 +1,7 @@
 class Dish < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :readytime, presence: true
+  validates :portions, presence: true
 end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "DB table" do
+    it { is_expected.to have_db_column :name }
+    it { is_expected.to have_db_column :description }
+    it { is_expected.to have_db_column :price }
+    it { is_expected.to have_db_column :readytime }
+    it { is_expected.to have_db_column :portions }
+  end
+
+  describe "Validation" do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_presence_of :price }
+    it { is_expected.to validate_presence_of :readytime }
+    it { is_expected.to validate_presence_of :portions }
+  end
+
+  describe "Factory" do
+    it 'should have a valid factory' do
+      expect(FactoryGirl.create(:dish)).to be_valid
+    end
+  end
 end
