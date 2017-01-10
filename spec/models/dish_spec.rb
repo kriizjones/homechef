cdb64c052551fb30b11e7829eda334f715ab6f41
aniansson <ebba@aniansson.se>
2017-01-10 20:13:08 +0100

    Syntax change

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
 
-  describe "DB table" do
+  describe "DB tables" do
     it { is_expected.to have_db_column :name }
     it { is_expected.to have_db_column :description }
     it { is_expected.to have_db_column :price }
@@ -10,7 +10,7 @@ RSpec.describe Dish, type: :model do
     it { is_expected.to have_db_column :portions }
   end
 
-  describe "Validation" do
+  describe "Validations" do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :description }
     it { is_expected.to validate_presence_of :price }
