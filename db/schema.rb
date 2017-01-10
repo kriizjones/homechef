1e4ba57eaf5cabd0c2c43136cb01902570338246
tochman <thomas@craftacademy.se>
2017-01-10 21:43:33 +0100

    adds migration to rename attribute name formats code on dishes/index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110153243) do
+ActiveRecord::Schema.define(version: 20170110203947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20170110153243) do
     t.string   "name"
     t.text     "description"
     t.integer  "price"
-    t.datetime "readytime"
+    t.datetime "ready_time"
     t.integer  "portions"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
