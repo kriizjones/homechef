b748b31742c618bb4b78a72043d8727343e6b43d
rmzse <rodrigo@therawdelicious.com>
2017-01-10 23:05:08 +0100

    add test to check for placeholder div  for dishes in general, test failing

--- features/main_landing_page.feature
+++ features/main_landing_page.feature
@@ -7,3 +7,4 @@ Feature: Main landing page
     When I visit the main landing page
     Then I should see "Welcome to HomeChef"
     And I should see input of type "text" with name "enter_location"
+    And I should see a placeholder for dishes in general with an id of "random_dishes"
--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -5,3 +5,7 @@ end
 Then(/^I should see input of type "([^"]*)" with name "([^"]*)"$/) do |type, name|
   expect(page).to have_css('input[type='"#{type}"'][name*='"#{name}"']')
 end
+
+Then(/^I should see a placeholder for dishes in general with an id of "([^"]*)"$/) do |id|
+expect(page).to have_selector(:css, "div##{id}")
+end
