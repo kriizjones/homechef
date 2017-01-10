1c04d2f68f00abd2e46a36dcd5b50f32623173d9
kriizjones <kristoffer.s.karlsson@hotmail.com>
2017-01-10 21:00:17 +0100

    completed test to search location, the test now passes

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -2,6 +2,6 @@ Then(/^I should see "([^"]*)"$/) do |content|
   expect(page).to have_content content
 end
 
-Then(/^I should see input of type text$/) do
-  expect(page).to have_css('input[type="text"][name*="enter_location"]')
+Then(/^I should see input of type "([^"]*)" with name "([^"]*)"$/) do |type, name|
+  expect(page).to have_css('input[type='"#{type}"'][name*='"#{name}"']')
 end
