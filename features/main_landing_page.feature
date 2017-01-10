b8e0c1320622457599e5aa81c653153347a0cbcc
kriizjones <kristoffer.s.karlsson@hotmail.com>
2017-01-10 20:46:22 +0100

    test for search text and failed

--- features/main_landing_page.feature
+++ features/main_landing_page.feature
@@ -6,3 +6,4 @@ Feature: Main landing page
   Scenario: Visitor visits main page
     When I visit the main landing page
     Then I should see "Welcome to HomeChef"
+    And I should see input of type text
--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -1,3 +1,7 @@
 Then(/^I should see "([^"]*)"$/) do |content|
   expect(page).to have_content content
 end
+
+Then(/^I should see input of type text$/) do
+  expect(page).to have_css('input[type="text"][name*="enter_location"]')
+end
