b500929bfe21f2770e60cc27fde155b8296553f7
kriizjones <kristoffer.s.karlsson@hotmail.com>
2017-01-13 13:58:58 +0100

    test DB table, id and email success test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe 'DB table' do
+      it { is_expected.to have_db_column :id }
+      it { is_expected.to have_db_column :email }
+    end
 end
