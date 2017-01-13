86375bfa346c90750f32c13afc262d31aeccc15a
Philip <Philip.zdb@gmail.com>
2017-01-13 11:34:06 +0100

    Adds action unit tests for order_item

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
+
   describe "DB tables" do
     it { is_expected.to have_db_column :owner_id }
     it { is_expected.to have_db_column :owner_type }
@@ -9,6 +10,13 @@ RSpec.describe OrderItem, type: :model do
     it { is_expected.to have_db_column :item_type }
     it { is_expected.to have_db_column :price_cents }
     it { is_expected.to have_db_column :price_currency }
+  end
+
+  describe "Actions" do
+    it { is_expected.to respond_to :owner }
+    it { is_expected.to respond_to :quantity }
+    it { is_expected.to respond_to :item }
+    it { is_expected.to respond_to :price }
   end
 
   describe "Associations" do
