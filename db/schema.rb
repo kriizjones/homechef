ebf8c48cea3787d892f83016f1a5c263c409d5e8
kriizjones <kristoffer.s.karlsson@hotmail.com>
2017-01-13 12:31:17 +0100

    generate devise user model and migration, test still fail

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110203947) do
+ActiveRecord::Schema.define(version: 20170113112438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "buyingorders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dishes", force: :cascade do |t|
     t.string   "name"
@@ -21,8 +26,42 @@ ActiveRecord::Schema.define(version: 20170110203947) do
     t.integer  "price"
     t.datetime "ready_time"
     t.integer  "portions"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "orderitem_id"
+    t.index ["orderitem_id"], name: "index_dishes_on_orderitem_id", using: :btree
   end
 
+  create_table "orderitems", force: :cascade do |t|
+    t.datetime "pickup_time"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.integer  "buyingorder_id"
+    t.index ["buyingorder_id"], name: "index_orderitems_on_buyingorder_id", using: :btree
+  end
+
+  create_table "sellingorders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  add_foreign_key "dishes", "orderitems"
+  add_foreign_key "orderitems", "buyingorders"
 end
