38f14885add21dd6e613c65f7babcbd54410be9c
kriizjones <kristoffer.s.karlsson@hotmail.com>
2017-01-13 14:09:44 +0100

    unit test for reset password sent at, passing test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe User, type: :model do
     describe 'DB table' do
       it { is_expected.to have_db_column :id }
       it { is_expected.to have_db_column :email }
-      it { is_expected.to have_db_column :encrypted_password}
-      it { is_expected.to have_db_column :reset_password_token}
+      it { is_expected.to have_db_column :encrypted_password }
+      it { is_expected.to have_db_column :reset_password_token }
+      it { is_expected.to have_db_column :reset_password_sent_at }
       
+
     end
 end
