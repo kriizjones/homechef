3b0febd99ee38ea381d137c636a5e645946ea0f6
Philip <Philip.zdb@gmail.com>
2017-01-13 11:22:54 +0100

    Adds unit tests for order_items, fixes order_spec

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "DB tables" do
+    it { is_expected.to have_db_column :owner_id }
+    it { is_expected.to have_db_column :owner_type }
+    it { is_expected.to have_db_column :quantity }
+    it { is_expected.to have_db_column :item_id }
+    it { is_expected.to have_db_column :item_type }
+    it { is_expected.to have_db_column :price_cents }
+    it { is_expected.to have_db_column :price_currency }
+  end
+
+  describe "Associations" do
+    it { is_expected.to belong_to :item }
+  end
 end
