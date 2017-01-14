f0c6b0567853c8d7be9d3e7d4ce1f55dc69d75b6
aniansson <ebba@aniansson.se>
2017-01-14 20:05:42 +0100

    Add helper method for destroy

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,4 +3,12 @@ class CheckoutController < ApplicationController
     @dishes = Order.last.shopping_cart_items.all
     @total_amount = Order.last.total
   end
+
+  def destroy
+    order = Order.last
+    dish = Dish.find(params[:dish_id])
+    order.remove(dish)
+    flash[:notice] = "Removed dish from order"
+    redirect_back(fallback_location: checkout_index_path)
+  end
 end
