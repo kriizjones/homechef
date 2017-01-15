0edd7a03e7d3980ca57c4bccd6ef991c147d0fb7
rmzse <rodrigo@therawdelicious.com>
2017-01-15 17:12:14 +0100

    migrate new changes to user model

--- db/schema.rb
+++ db/schema.rb
@@ -15,34 +15,14 @@ ActiveRecord::Schema.define(version: 20170113112438) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "buyingorders", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "dishes", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "price"
     t.datetime "ready_time"
     t.integer  "portions"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.integer  "orderitem_id"
-    t.index ["orderitem_id"], name: "index_dishes_on_orderitem_id", using: :btree
-  end
-
-  create_table "orderitems", force: :cascade do |t|
-    t.datetime "pickup_time"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.integer  "buyingorder_id"
-    t.index ["buyingorder_id"], name: "index_orderitems_on_buyingorder_id", using: :btree
-  end
-
-  create_table "sellingorders", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,6 +42,4 @@ ActiveRecord::Schema.define(version: 20170113112438) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "dishes", "orderitems"
-  add_foreign_key "orderitems", "buyingorders"
 end
