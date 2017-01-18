c9908281a28c310f0a23eef0302ccde3c6def696
kriizjones <kristoffer.s.karlsson@hotmail.com>
2017-01-18 16:44:03 +0100

    added data

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,5 +17,15 @@ RSpec.describe User, type: :model do
 
     describe 'Validations' do
       it { is_expected.to validate_presence_of :email }
+      it { is_expected.to validate_presence_of(:encrypted_password) }
+      it { is_expected.to validate_presence_of :reset_password_token }
+      it { is_expected.to validate_presence_of :reset_password_sent_at }
+      it { is_expected.to validate_presence_of :remember_created_at }
+      it { is_expected.to validate_presence_of :sign_in_count }
+      it { is_expected.to validate_presence_of :current_sign_in_at }
+      it { is_expected.to validate_presence_of :last_sign_in_at }
+      it { is_expected.to validate_presence_of :current_sign_in_ip }
+      it { is_expected.to validate_presence_of :last_sign_in_ip }
     end
+
 end
