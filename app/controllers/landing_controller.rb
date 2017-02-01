c2378bde76af5828f693ae60974da81d75b37df2
aniansson <ebba@aniansson.se>
2017-02-01 14:31:33 +0100

    Fix issue with goecoding (#22)
    * Add else and if method
    * Delete unecessary method: act_as_mappable on user
    * Put back act_as_mappable

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,8 +2,13 @@ class LandingController < ApplicationController
   geocode_ip_address
 
   def index
-    lat = session[:geo_location]['lat']
-    lng = session[:geo_location]['lng']
+    if session[:geo_location].is_a?(Hash)
+      lat = session[:geo_location]['lat']
+      lng = session[:geo_location]['lng']
+    else
+      lat = session[:geo_location].lat
+      lng = session[:geo_location].lng
+    end
     @dishes = Dish.within(4, origin: [lat,lng])
   end
 end
