81ed855e208f9985147d382be6c101608a3b4db6
kriizjones <kristoffer.s.karlsson@hotmail.com>
2017-02-01 09:59:53 +0100

    start and delete the files to start a new branch

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   resources :charges, only: [:create]
   post :add_to_order, controller: :orders, action: :add_to_order
   post :remove_from_order, controller: :checkout, action: :delete
+
+
+# You can have the root of your site routed with "root"
+  root 'home#index'
+  get 'sign-up' > 'home#sign_up'
+
 end
